afa1284a54477136d7ebc9904fdaba21b3722a50
dejiobasan <dejiobasan@yahoo.com>
2023-09-19 22:08:15 +0100

    .

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+// import Usinghooks from "./Usinghooks"
+import Usingcomponents from "./Usingcomponents"
+
+function App() {
+  return (
+   <div className='App'>
+      <div className='App-header'>
+        <h1>Welcome</h1>
+        <Usingcomponents />
+      </div>
+   </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Usingcomponents.jsx
@@ -0,0 +1,40 @@
+// import React from "react";
+import { FlutterWaveButton, closePaymentModal } from "flutterwave-react-v3";
+
+export default function App() {
+  const config = {
+    // process.env.REACT_APP_PUBLIC_KEY
+    public_key: "",
+    tx_ref: Date.now(),
+    amount: 100,
+    currency: "NGN",
+    payment_options: "card,mobilemoney,ussd",
+    customer: {
+      email: "user@gmail.com",
+      phone_number: "070********",
+      name: "john doe",
+    },
+    customizations: {
+      title: "Test Payment Gateway",
+      description: "Test Payment",
+      logo: "https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg",
+    },
+  };
+
+  const fwConfig = {
+    ...config,
+    text: "Pay now!",
+    callback: (response) => {
+      console.log(response);
+      closePaymentModal(); // this will close the modal programmatically
+    },
+    onClose: () => {},
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello Test user</h1>
+      <FlutterWaveButton {...fwConfig} />
+    </div>
+  );
+}
